d9b86e43dfd852d3e78493216cb21a0443b8ee72
Sathish <shanmukhauppalapati11@gmail.com>
2024-03-30 23:30:21 +0530

    removed crop activity, updated ml api.

---
+++ app/src/main/java/com/teaminversion/envisionbuddy/CropperActivity.java
@@ -0,0 +1,72 @@
+package com.teaminversion.envisionbuddy;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.yalantis.ucrop.UCrop;
+
+import java.io.File;
+import java.util.UUID;
+
+public class CropperActivity extends AppCompatActivity {
+
+    String result;
+    Uri fileUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_cropper);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        readIntent();
+
+        String destUri = new StringBuilder(UUID.randomUUID().toString()).append(".jpg").toString();
+
+        UCrop.Options options = new UCrop.Options();
+        options.setShowCropFrame(true);
+
+        UCrop.of(fileUri, Uri.fromFile(new File(getCacheDir(),destUri)))
+                .withOptions(options)
+                .withAspectRatio(0,0)
+                .useSourceImageAspectRatio()
+                .withMaxResultSize(2000,2000)
+                .start(CropperActivity.this);
+
+    }
+
+    private void readIntent() {
+        Intent intent = getIntent();
+        if(intent.getExtras()!=null){
+            result = intent.getStringExtra("DATA");
+            fileUri = Uri.parse(result);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK && requestCode==UCrop.REQUEST_CROP) {
+            final Uri resultUri = UCrop.getOutput(data);
+            Intent returnIntent = new Intent();
+            returnIntent.putExtra("RESULT", resultUri+"");
+            setResult(-1, returnIntent);
+            finish();
+        }
+        else if (resultCode==UCrop.RESULT_ERROR){
+            final Throwable cropError = UCrop.getError(data);
+        }
+    }
+}
