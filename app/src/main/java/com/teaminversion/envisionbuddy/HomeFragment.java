4e049fc334ed48893ce70e59c902272f9e1d7d34
Sathish <shanmukhauppalapati11@gmail.com>
2024-03-30 23:30:03 +0530

    removed crop activity, updated ml api.

--- app/src/main/java/com/teaminversion/envisionbuddy/HomeFragment.java
+++ app/src/main/java/com/teaminversion/envisionbuddy/HomeFragment.java
@@ -1,7 +1,12 @@
 package com.teaminversion.envisionbuddy;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
@@ -21,14 +26,20 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.core.content.FileProvider;
 
+import com.canhub.cropper.CropImageContract;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.mlkit.vision.common.InputImage;
@@ -36,8 +47,10 @@ import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
-import com.theartofdev.edmodo.cropper.CropImage;
-import com.theartofdev.edmodo.cropper.CropImageView;
+import com.canhub.cropper.CropImage;
+import com.canhub.cropper.CropImageView;
+import com.canhub.cropper.CropImageActivity;
+import com.yalantis.ucrop.UCrop;
 
 import java.io.File;
 import java.io.IOException;
@@ -46,22 +59,54 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Map;
 
+import javax.xml.transform.Result;
+
 public class HomeFragment extends Fragment {
 
     String mCurrentPhotoPath;
     static MainRecyclerViewAdapter mainAdapter;
-    private static final int MY_CAMERA_PERMISSION_CODE = 100;
-    private static final int MY_STORAGE_PERMISSION_CODE = 101;
-    private static final int REQUEST_IMAGE_CAPTURE = 1;
-    /*private static final int CAMERA_REQUEST_CODE = 100;
-    private static final int CROP_IMAGE_REQUEST_CODE = 200;
-    private static final int STORAGE_PERMISSION_CODE = 300;
-    private Uri photoUri;*/
+    private static final int REQUEST_CAMERA_PERMISSION = 101;
+    private static final int REQUEST_GALLERY_PERMISSION = 102;
+    private static final int REQUEST_IMAGE_CAPTURE = 103;
+    private static final int REQUEST_PICK_IMAGE = 104;
+    private Uri imageUri;
+
+    private void showImageSourceDialog() {
+        CharSequence[] options = {"Camera", "Gallery"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Choose an option");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:
+                        if (checkCameraPermission()) {
+                            dispatchTakePictureIntent();
+                        } else {
+                            requestCameraPermission();
+                        }
+                        break;
+                    case 1:
+                        if (checkGalleryPermission()) {
+                            openGallery();
+                        } else {
+                            requestGalleryPermission();
+                        }
+                        break;
+                }
+            }
+        });
+        builder.show();
+    }
+
+
     private File photoFile;
     static ArrayList<Map<String, String>> recentList = new ArrayList<>();
     static String resultText;
     Button continueButton;
     TextView textView;
+    ActivityResultLauncher<String> mGetontent;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
@@ -87,9 +132,12 @@ public class HomeFragment extends Fragment {
         FloatingActionButton scanButton = root.findViewById(R.id.scanButton);
         scanButton.setOnClickListener(v -> {
             if (getActivity().checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+                requestPermissions(new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
             } else {
-                dispatchTakePictureIntent();
+                showImageSourceDialog();
+                //dispatchTakePictureIntent();
+                //mGetontent.launch("image/*");
+
             }
             /*if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
                     ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
@@ -99,6 +147,15 @@ public class HomeFragment extends Fragment {
             }*/
 
         });
+//        mGetontent = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+//            @Override
+//            public void onActivityResult(Uri o) {
+//                Intent intent = new Intent(getContext(),CropperActivity.class);
+//                intent.putExtra("DATA", resultText.toString());
+//                startActivityForResult(intent,101);
+//            }
+//        });
+
 
         ImageButton clearImageButton = root.findViewById(R.id.clearImageButton);
         clearImageButton.setOnClickListener(new View.OnClickListener() {
@@ -117,6 +174,7 @@ public class HomeFragment extends Fragment {
         return root;
     }
 
+
     private File createImageFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
@@ -129,42 +187,88 @@ public class HomeFragment extends Fragment {
     }
 
     private void dispatchTakePictureIntent() {
-        if (getActivity().checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-
-            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_STORAGE_PERMISSION_CODE);
-        } else {
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-            if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
-                photoFile = null;
-                try {
-                    photoFile = createImageFile();
-                    Log.d("ImageFile", "Created");
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-
-                if (photoFile != null) {
-                    CropImage.activity()
-                            .setGuidelines(CropImageView.Guidelines.ON)
-                            .start(getContext(),this);
-                }
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
             }
-
+            if (photoFile != null) {
+                imageUri = FileProvider.getUriForFile(getActivity(), getActivity().getApplicationContext().getPackageName() + ".provider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
         }
+//        if (getActivity().checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+//
+//            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_STORAGE_PERMISSION_CODE);
+//        } else {
+//            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//
+//            if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+//                photoFile = null;
+//                try {
+//                    photoFile = createImageFile();
+//                    Log.d("ImageFile", "Created");
+//                } catch (IOException ex) {
+//                    ex.printStackTrace();
+//                }
+//
+//                if (photoFile != null) {
+////                    CropImage.activity()
+////                            .setGuidelines(CropImageView.Guidelines.ON)
+////                            .start(getContext(),this);
+//
+//
+//                }
+//            }
+//
+//        }
+//        Intent i = new Intent();
+//        i.setType("image/*");
+//        i.setAction(Intent.ACTION_GET_CONTENT);
+//
+//        // pass the constant to compare it
+//        // with the returned requestCode
+//        startActivityForResult(Intent.createChooser(i, "Select Picture"), 200);
+    }
+    private void openGallery() {
+        Intent pickPhotoIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(pickPhotoIntent, REQUEST_PICK_IMAGE);
+    }
+    // Method to check camera permission
+    private boolean checkCameraPermission() {
+        return ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    // Method to request camera permission
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+    }
+
+    // Method to check gallery permission
+    private boolean checkGalleryPermission() {
+        return ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    // Method to request gallery permission
+    private void requestGalleryPermission() {
+        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_GALLERY_PERMISSION);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == MY_CAMERA_PERMISSION_CODE) {
+        if (requestCode == REQUEST_CAMERA_PERMISSION) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(getActivity(), "Camera permission granted", Toast.LENGTH_LONG).show();
                 dispatchTakePictureIntent();
             } else {
                 Toast.makeText(getActivity(), "Camera permission denied", Toast.LENGTH_LONG).show();
             }
-        } else if (requestCode == MY_STORAGE_PERMISSION_CODE) {
+        } else if (requestCode == REQUEST_GALLERY_PERMISSION) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(getActivity(), "Storage permission granted", Toast.LENGTH_LONG).show();
                 dispatchTakePictureIntent();
@@ -181,33 +285,79 @@ public class HomeFragment extends Fragment {
         Log.d("requestCode", String.valueOf(requestCode));
         Log.d("resultCode", String.valueOf(resultCode));
         try {
-            if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-                //File file = new File(mCurrentPhotoPath);
-                //Log.e("ImageReg", "Cropping activity");
-                CropImage.ActivityResult cropResult = CropImage.getActivityResult(data);
-                if (resultCode == getActivity().RESULT_OK) {
-                    Uri resultUri = cropResult.getUri();
-                    Log.d("CropResult URI", String.valueOf(resultUri));
-                    Bitmap bitmap = null;
-                    try {
-                        bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), resultUri);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    if (bitmap != null) {
-                        InputImage image = InputImage.fromBitmap(bitmap, 0);
-                        TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
-                        //TextRecognizer recognizer = TextRecognition.getClient();
-                        Task<Text> result = recognizer.process(image)
-                                .addOnSuccessListener(visionText -> processTextBlock(visionText))
-                                .addOnFailureListener(e -> Toast.makeText(getActivity(), "Failed", Toast.LENGTH_SHORT).show());
-                    }
-                } else {
-                    Log.d("CropResult", "resultCode is not RESULT_OK");
-                    //Toast.makeText(getActivity(), "Crop failed: " + cropResult.getError(), Toast.LENGTH_SHORT).show();
+            if (resultCode == Activity.RESULT_OK) {
+                switch (requestCode) {
+                    case REQUEST_IMAGE_CAPTURE:
+                        // Image captured from camera
+                        // Use the imageUri here
+                        Bitmap bitmap1 = null;
+                        try {
+                            bitmap1 = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), imageUri);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        if (bitmap1 != null) {
+                            InputImage image = InputImage.fromBitmap(bitmap1, 0);
+                            TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+                            //TextRecognizer recognizer = TextRecognition.getClient();
+                            Task<Text> result = recognizer.process(image)
+                                    .addOnSuccessListener(visionText -> processTextBlock(visionText))
+                                    .addOnFailureListener(e -> Toast.makeText(getActivity(), "Failed", Toast.LENGTH_SHORT).show());
+                        }
+                        //Toast.makeText(getActivity(), "Image captured from camera: " + imageUri.toString(), Toast.LENGTH_SHORT).show();
+                        break;
+                    case REQUEST_PICK_IMAGE:
+                        // Image picked from gallery
+                        Uri resultUri = data.getData();
+                        // Use the selectedImage Uri here
+                        Bitmap bitmap2 = null;
+                        try {
+                            bitmap2 = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), resultUri);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        if (bitmap2 != null) {
+                            InputImage image = InputImage.fromBitmap(bitmap2, 0);
+                            TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+                            //TextRecognizer recognizer = TextRecognition.getClient();
+                            Task<Text> result = recognizer.process(image)
+                                    .addOnSuccessListener(visionText -> processTextBlock(visionText))
+                                    .addOnFailureListener(e -> Toast.makeText(getActivity(), "Failed", Toast.LENGTH_SHORT).show());
+                        }
+                        //Toast.makeText(getActivity(), "Image picked from gallery: " + resultUri.toString(), Toast.LENGTH_SHORT).show();
+                        break;
                 }
-
             }
+            else {
+                Log.d("CropResult", "resultCode is not RESULT_OK");
+                //Toast.makeText(getActivity(), "Crop failed: " + cropResult.getError(), Toast.LENGTH_SHORT).show();
+            }
+//            if(resultCode== RESULT_OK && requestCode==200) {
+//                //String resultString = data.getStringExtra("RESULT");
+//                Uri resultUri = data.getData();;
+//                if(resultUri!=null) {
+//                    //resultUri = Uri.parse(resultString);
+//                    Log.d("CropResult URI", String.valueOf(resultUri));
+//                    Bitmap bitmap = null;
+//                    try {
+//                        bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), resultUri);
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                    if (bitmap != null) {
+//                        InputImage image = InputImage.fromBitmap(bitmap, 0);
+//                        TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+//                        //TextRecognizer recognizer = TextRecognition.getClient();
+//                        Task<Text> result = recognizer.process(image)
+//                                .addOnSuccessListener(visionText -> processTextBlock(visionText))
+//                                .addOnFailureListener(e -> Toast.makeText(getActivity(), "Failed", Toast.LENGTH_SHORT).show());
+//                    }
+//                }
+//            }
+//            else {
+//                Log.d("CropResult", "resultCode is not RESULT_OK");
+//                //Toast.makeText(getActivity(), "Crop failed: " + cropResult.getError(), Toast.LENGTH_SHORT).show();
+//            }
         } catch (Exception error)
         {
             error.printStackTrace();
